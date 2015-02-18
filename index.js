d0e0e7f0a27b67618a7600dec2b0a834959035a6
Masaaki Morishita <ikasama3.0@gmail.com>
2015-02-18 19:35:08 +0900

    first implement

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = 
+var postcss = require('postcss')
+
+module.exports = function plugin (options) {
+    options = options = {}
+
+    return function (root) {
+        var tmpSelectors = []
+        root.eachRule(function (rule) {
+            rule.selectors.forEach(function (selector) {
+                if (checkSelector(selector)) {
+                    tmpSelectors.forEach(function (tmp) {
+                        if (tmp === selector) {
+                            throw Error('NoCSS: cannot overwrite any rule sets')
+                        }
+                        else {
+                            tmpSelectors.push(selector)
+                        }
+                    })
+                }
+                else {
+                    throw Error('NoCSS: can use only class selectors and cannot nest selectors')
+                }
+            })
+        })
+
+        return root
+    }
+}
+
+function checkSelector (selector) {
+    if (!selector.match(/^(\.(\w|\-|:)+)$/)) return false
+    return true
+}
--- test.js
+++ test.js
@@ -1,1 +1,12 @@
+var fs = require('fs')
 var nocssLint = require('./')
+var postcss = require('postcss')
+var test = require('tape')
+
+var css = fs.readFileSync('fixtures.css', 'utf-8').trim()
+
+test('test-1', function (t) {
+    var actual = postcss().use(nocssLint()).process(css).css.trim()
+    t.same(actual)
+    t.end()
+})
